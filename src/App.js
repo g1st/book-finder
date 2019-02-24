2b81d4bcf063bcbdda0c9f53705ff9742ba7fcdd
g1st <gintstan@gmail.com>
2019-02-24 20:29:26 +0000

    Add a bit of error checing. Still faulty.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,21 @@ class App extends Component {
     searchTerm: '',
     books: null,
     startIndex: 0,
+    error: null,
     loading: false
   };
 
   handleInput = event => {
     this.setState({ searchTerm: event.target.value });
+  };
+
+  checkInput = input => {
+    if (input.length < 1) {
+      this.setState({ error: 'Your input is empty.' });
+      return true;
+    }
+    this.setState({ error: null });
+    return false;
   };
 
   getBooks = (searchTerm, startIndex) => {
@@ -24,8 +34,16 @@ class App extends Component {
       `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&startIndex=${startIndex}&maxResults=10&fields=items(selfLink)&key=${GOOGLE_BOOKS_API_KEY}`
     )
       .then(response => response.json())
-      .then(data =>
-        Promise.all(
+      .then(data => {
+        if (data.error && data.error.errors.length > 0) {
+          throw Error(data.error.message.split('. ')[0]);
+        }
+
+        if (Object.entries(data).length === 0 && data.constructor === Object) {
+          throw Error('No matches found.');
+        }
+
+        return Promise.all(
           data.items.map(book =>
             fetch(
               `${
@@ -33,10 +51,14 @@ class App extends Component {
               }?fields=id,volumeInfo(title,authors,publisher,previewLink,imageLinks(thumbnail,small))&key=${GOOGLE_BOOKS_API_KEY}`
             )
           )
-        )
-      )
+        );
+      })
       .then(res => Promise.all(res.map(book => book.json())))
       .then(books => {
+        console.log(books);
+
+        if (books.length < 1) throw Error('No books found.');
+
         this.setState({ loading: false });
         return books.map(book => ({
           id: book.id,
@@ -49,21 +71,26 @@ class App extends Component {
         }));
       })
       .catch(err => {
-        console.error(err);
+        console.log(err);
 
-        this.setState({ loading: false });
+        this.setState({
+          loading: false,
+          error: err.message || 'Sorry we cannot process your request.'
+        });
       });
   };
 
   handleSubmit = event => {
     event.preventDefault();
     const { searchTerm, startIndex, maxResults } = this.state;
+    if (this.checkInput(searchTerm)) return;
     this.getBooks(searchTerm, startIndex, maxResults).then(books =>
       this.setState({ books })
     );
   };
 
   handleNextPage = () => {
+    if (this.checkInput(this.state.searchTerm)) return;
     this.setState(
       prevState => ({
         startIndex: prevState.startIndex + 10
@@ -79,6 +106,7 @@ class App extends Component {
   };
 
   handlePreviousPage = () => {
+    if (this.checkInput(this.state.searchTerm)) return;
     if (this.state.startIndex < 1) return;
 
     this.setState(
@@ -96,32 +124,64 @@ class App extends Component {
   };
 
   render() {
+    const bookList = (
+      <div className="books-item">
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <BooksList data={this.state.books} />
+            <div className="pagination">
+              {this.state.startIndex > 1 ? (
+                <span
+                  className="pagination-navigation"
+                  onClick={this.handlePreviousPage}
+                >
+                  Back
+                </span>
+              ) : null}
+              {this.state.books ? (
+                <>
+                  <span className="page">
+                    Page {this.state.startIndex / 10 + 1}
+                  </span>
+                  <span
+                    className="pagination-navigation"
+                    onClick={this.handleNextPage}
+                  >
+                    Next
+                  </span>
+                </>
+              ) : null}
+            </div>
+          </>
+        )}
+      </div>
+    );
+
+    const error = <div className="error">{this.state.error}</div>;
+
     return (
       <div>
-        <main>
-          <h1>Book Finder</h1>
-          <Search
-            handleInput={this.handleInput}
-            handleSubmit={this.handleSubmit}
-          />
-          {this.state.loading ? (
-            <div>Loading...</div>
-          ) : (
-            <>
-              <div>
-                {this.state.startIndex > 1 ? (
-                  <span onClick={this.handlePreviousPage}>Back</span>
-                ) : null}
-                {this.state.books ? (
-                  <>
-                    <span>Page {this.state.startIndex / 10 + 1}</span>
-                    <span onClick={this.handleNextPage}>Next</span>
-                  </>
-                ) : null}
-              </div>
-              <BooksList data={this.state.books} />
-            </>
-          )}
+        <main className="grid">
+          <div className="search-item">
+            <h1 className="heading">Book Finder</h1>
+            <Search
+              handleInput={this.handleInput}
+              handleSubmit={this.handleSubmit}
+            />
+          </div>
+          {this.state.error ? error : bookList}
+          <footer className="footer-item">
+            <a
+              className="footer-link"
+              href="http://github.com/g1st/book-finder"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @g1st
+            </a>
+          </footer>
         </main>
       </div>
     );
