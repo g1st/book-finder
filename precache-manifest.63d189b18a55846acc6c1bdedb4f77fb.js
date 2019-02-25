b5058868abf39d54c992a7de0adee6adf1a5d497
g1st <gintstan@gmail.com>
2019-02-25 19:47:03 +0000

    Updates

---
+++ precache-manifest.63d189b18a55846acc6c1bdedb4f77fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b570893abe49d240a24",
+    "url": "/book-finder/static/css/main.9301fc26.chunk.css"
+  },
+  {
+    "revision": "1b570893abe49d240a24",
+    "url": "/book-finder/static/js/main.1b570893.chunk.js"
+  },
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/book-finder/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "caf9523fc16518578e02d8a6557676b1",
+    "url": "/book-finder/static/media/fallbackImage.caf9523f.jpg"
+  },
+  {
+    "revision": "413a56155a4693164ae2331caa2a7a57",
+    "url": "/book-finder/index.html"
+  }
+];
